1a197f6f3a59b47d41372f1ca794600d1c6fbe5d
rmanaem <rmanaem@protonmail.ch>
2025-02-10 16:08:08 -0500

    Updated dummy e2e test

--- cypress/e2e/Dummy.cy.ts
+++ cypress/e2e/Dummy.cy.ts
@@ -1,7 +1,6 @@
 describe('Dummy', () => {
   it('tests', () => {
     cy.visit('http://localhost:5173');
-    cy.contains('count').click();
-    cy.contains('count').should('contain', '1');
+    cy.contains('Hello World');
   });
 });
