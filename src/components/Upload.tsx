b26b3c18c1a54051e09b6aa86cd8623ac3749f43
rmanaem <rmanaem@protonmail.ch>
2025-02-12 15:15:37 -0500

    Implemented `Upload` component

---
+++ src/components/Upload.tsx
@@ -0,0 +1,15 @@
+import NavigationButton from './NavigationButton';
+
+function Upload() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1>Upload</h1>
+      <div className="flex space-x-4">
+        <NavigationButton label="Back - Welcome" viewToNavigateTo="welcome" />
+        <NavigationButton label="Next - Column Annotation" viewToNavigateTo="columnAnnotation" />
+      </div>
+    </div>
+  );
+}
+
+export default Upload;
