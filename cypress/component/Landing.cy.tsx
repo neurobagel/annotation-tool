23a70fa322599aacca0256e70e2d3a9b4492f210
rmanaem <rmanaem@protonmail.ch>
2025-02-13 13:45:47 -0500

    Added assertions for the content of `Landing` component

--- cypress/component/Landing.cy.tsx
+++ cypress/component/Landing.cy.tsx
@@ -1,8 +1,13 @@
 import Landing from '../../src/components/Landing';
 
 describe('Landing', () => {
-  it('should render correctly', () => {
+  it('should render the content of Landing view  correctly', () => {
     cy.mount(<Landing />);
-    cy.contains('Welcome');
+    cy.contains('Welcome to the Neurobagel Annotation Tool');
+    cy.get('img[alt="Neurobagel Logo"]').should('be.visible');
+    cy.get('img[alt="Neurobagel Logo"]').should('have.attr', 'alt', 'Neurobagel Logo');
+    cy.get('img[alt="Landing emoji"]').should('be.visible');
+    cy.get('img[alt="Landing emoji"]').should('have.attr', 'alt', 'Landing emoji');
+    cy.get('[data-cy="get started-button"]').should('be.visible');
   });
 });
