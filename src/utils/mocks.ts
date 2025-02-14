6a8061720e3b6739e3a8950b3e590791ebf2cf02
rmanaem <rmanaem@protonmail.ch>
2025-02-14 15:35:36 -0500

    Added mocks for the unit tests

---
+++ src/utils/mocks.ts
@@ -0,0 +1,17 @@
+export const mockDataTable = {
+  '1': ['sub-718211', 'sub-718213', 'sub-718216'],
+  '2': ['28.4', '24.6', '43.6'],
+  '3': ['M', 'F', 'M'],
+};
+
+export const mockHeaders = {
+  '1': {
+    header: 'participant_id',
+  },
+  '2': {
+    header: 'age',
+  },
+  '3': {
+    header: 'sex',
+  },
+};
