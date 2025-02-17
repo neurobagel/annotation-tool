f85e6e094843d9aff3e59661cb5a9289cb0a66c3
rmanaem <rmanaem@protonmail.ch>
2025-02-17 18:02:38 -0500

    Implemented component test for `FileUploader`

---
+++ cypress/component/FileUploader.cy.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import FileUploader from '../../src/components/FileUploader';
+
+const props = {
+  handleClickToUpload: () => {},
+  handleDrop: () => {},
+  handleDragOver: () => {},
+  handleFileUpload: () => {},
+  fileInputRef: React.createRef<HTMLInputElement>(),
+  allowedFileType: '.csv',
+};
+
+describe('FileUploader', () => {
+  it('should render the FileUploader component', () => {
+    cy.mount(
+      <FileUploader
+        handleClickToUpload={props.handleClickToUpload}
+        handleDrop={props.handleDrop}
+        handleDragOver={props.handleDragOver}
+        handleFileUpload={props.handleFileUpload}
+        fileInputRef={props.fileInputRef}
+        allowedFileType={props.allowedFileType}
+      />
+    );
+  });
+  cy.get('[data-cy="upload-area"]').should('be.visible');
+  cy.get('[data-cy="upload-area"]').should('contain', 'Upload your file (.csv)');
+});
