30dc0aba17714edfd0419c8053ec15e8ddcffee2
rmanaem <rmanaem@protonmail.ch>
2025-02-27 17:09:10 -0500

    Updated `Download` component test

--- cypress/component/Download.cy.tsx
+++ cypress/component/Download.cy.tsx
@@ -1,8 +1,80 @@
+import Ajv from 'ajv';
 import Download from '../../src/components/Download';
+import useDataStore from '../../src/stores/data';
+import {
+  columnsWithDescription,
+  columnsWithNoDescription,
+  mockDataDictionaryWithDescription,
+  mockDataDictionaryWithNoDescription,
+} from '../../src/utils/mocks';
+
+interface ValidateFunction {
+  (data: unknown): boolean;
+  errors?: { instancePath: string }[];
+}
 
 describe('Download', () => {
-  it('should render correctly', () => {
+  it('renders the component correctly', () => {
     cy.mount(<Download />);
-    cy.contains('Download');
+
+    cy.get('[data-cy="download"]').should('be.visible');
+    cy.get('[data-cy="complete-annotations-alert"] img')
+      .should('be.visible')
+      .and('have.attr', 'alt', 'bagel confetti');
+    cy.get('[data-cy="complete-annotations-alert"]')
+      .should('be.visible')
+      .and('contain', 'You have successfully created');
+    cy.get('[data-cy="data-dictionary-toggle-preview-button"]').should('be.visible');
+    cy.get('[data-cy="data-dictionary-preview"]').should('be.visible');
+    cy.get('[data-cy="data-dictionary-toggle-preview-button"]').click();
+    cy.get('[data-cy="data-dictionary-preview"]').should('not.be.visible');
+    cy.get('[data-cy="data-dictionary-next-steps-list"]')
+      .should('be.visible')
+      .and('contain', '.json data dictionary');
+    cy.get('[data-cy="download-data-dictionary-button"]').should('be.visible');
+  });
+
+  it('renders the component correctly when the data dictionary is invalid', () => {
+    const validateStub = cy.stub().returns(false) as ValidateFunction;
+    validateStub.errors = [{ instancePath: '/column1' }, { instancePath: '/column2' }];
+
+    cy.stub(Ajv.prototype, 'compile').callsFake(() => validateStub);
+
+    cy.mount(<Download />);
+
+    cy.get('[data-cy="download"]').should('be.visible');
+    cy.get('[data-cy="incomplete-annotations-alert"]')
+      .should('be.visible')
+      .and('contain', 'incomplete annotations');
+    cy.get('[data-cy="incomplete-annotations-list"]')
+      .should('be.visible')
+      .and('contain', 'column1');
+    cy.get('[data-cy="data-dictionary-preview"]').should('be.visible');
+    cy.get('[data-cy="download-data-dictionary-button"]').should('be.disabled');
+    cy.get('[data-cy="force-download-switch"]').should('be.visible');
+    cy.get('[data-cy="force-download-switch"]').click();
+    cy.get('[data-cy="download-data-dictionary-button"]').should('be.enabled');
+  });
+
+  it('generates the data dictionary file with descriptions when the download button is clicked', () => {
+    useDataStore.setState({ columns: columnsWithDescription });
+
+    cy.mount(<Download />);
+
+    cy.get('[data-cy="download-data-dictionary-button"]').click();
+    cy.readFile('cypress/downloads/neurobagel-data-dictionary.json').then((fileContent) => {
+      expect(fileContent).to.deep.equal(mockDataDictionaryWithDescription);
+    });
+  });
+
+  it('generates the data dictionary file with no descriptions when the download button is clicked', () => {
+    useDataStore.setState({ columns: columnsWithNoDescription });
+
+    cy.mount(<Download />);
+
+    cy.get('[data-cy="download-data-dictionary-button"]').click();
+    cy.readFile('cypress/downloads/neurobagel-data-dictionary.json').then((fileContent) => {
+      expect(fileContent).to.deep.equal(mockDataDictionaryWithNoDescription);
+    });
   });
 });
