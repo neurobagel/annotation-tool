ae1d6bcc77b6bf6911d5d172e58bcb56e146b335
rmanaem <rmanaem@protonmail.ch>
2025-02-27 17:07:14 -0500

    Removed persistence of current view

--- src/stores/view.ts
+++ src/stores/view.ts
@@ -1,22 +1,13 @@
 import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware';
 
 type ViewStore = {
   currentView: string;
   setCurrentView: (view: string) => void;
 };
 
-const useViewStore = create<ViewStore>()(
-  persist(
-    (set) => ({
-      currentView: 'landing',
-      setCurrentView: (view: string) => set({ currentView: view }),
-    }),
-    {
-      name: 'view-store',
-      storage: createJSONStorage(() => sessionStorage),
-    }
-  )
-);
+const useViewStore = create<ViewStore>()((set) => ({
+  currentView: 'landing',
+  setCurrentView: (view: string) => set({ currentView: view }),
+}));
 
 export default useViewStore;
