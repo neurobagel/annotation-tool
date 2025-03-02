2c484c147122cd29a93b4a855cc8691d8eb2e921
rmanaem <rmanaem@protonmail.ch>
2025-03-02 12:08:50 -0500

    Refactored `NavigationButton`

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -1,24 +1,55 @@
 import { Button } from '@mui/material';
 import useViewStore from '../stores/view';
 
+const defaultProps = {
+  backLabel: 'Back',
+  nextLabel: 'Next',
+  className: '',
+};
+
 function NavigationButton({
-  label,
-  viewToNavigateTo,
+  backView,
+  nextView,
+  backLabel,
+  nextLabel,
+  className,
 }: {
-  label: string;
-  viewToNavigateTo: string;
+  backView: string | undefined;
+  nextView: string | undefined;
+  backLabel?: string;
+  nextLabel?: string;
+  className?: string;
 }) {
   const setCurrentView = useViewStore((state) => state.setCurrentView);
 
-  const handleClick = () => {
-    setCurrentView(viewToNavigateTo);
+  const handleBack = () => {
+    if (backView) {
+      setCurrentView(backView);
+    }
+  };
+
+  const handleNext = () => {
+    if (nextView) {
+      setCurrentView(nextView);
+    }
   };
 
   return (
-    <Button data-cy={`${label.toLowerCase()}-button`} variant="contained" onClick={handleClick}>
-      {label}
-    </Button>
+    <div className={`flex flex-row justify-between ${className}`}>
+      {backView && (
+        <Button data-cy="back-button" variant="contained" onClick={handleBack}>
+          {backLabel}
+        </Button>
+      )}
+      {nextView && (
+        <Button data-cy="next-button" variant="contained" onClick={handleNext}>
+          {nextLabel}
+        </Button>
+      )}
+    </div>
   );
 }
+
+NavigationButton.defaultProps = defaultProps;
 
 export default NavigationButton;
