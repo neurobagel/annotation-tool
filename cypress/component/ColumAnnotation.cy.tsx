5802b076cff35868b70db0e43c494e48950a69c3
rmanaem <rmanaem@protonmail.ch>
2025-03-05 18:25:27 -0500

    Updated `ColumnAnnotation` component test

--- cypress/component/ColumAnnotation.cy.tsx
+++ cypress/component/ColumAnnotation.cy.tsx
@@ -1,11 +1,69 @@
 import ColumnAnnotation from '../../src/components/ColumnAnnotation';
+import useDataStore from '../../src/stores/data';
 
 describe('ColumnAnnotation', () => {
-  it('should render correctly', () => {
+  beforeEach(() => {
+    useDataStore.setState({
+      columns: {
+        1: {
+          header: 'some column',
+          description: 'This is some column',
+          dataType: 'Categorical',
+          standardizedVariable: { identifier: 'somevar', label: 'some variable' },
+        },
+        2: {
+          header: 'another column',
+          description: 'This is another column',
+          dataType: 'Continuous',
+          standardizedVariable: null,
+        },
+      },
+      standardizedVaribles: {
+        var1: {
+          identifier: 'somevar',
+          label: 'some variable',
+        },
+        var2: {
+          identifier: 'anothervar',
+          label: 'another variable',
+        },
+      },
+    });
+  });
+
+  it('renders the component correctly', () => {
     cy.mount(<ColumnAnnotation />);
     cy.contains('Column Annotation');
+    cy.get('[data-cy="1-column-annotation-card"]').should('be.visible');
+    cy.get('[data-cy="1-description"]').should('be.visible').and('contain', 'This is some column');
+    cy.get('[data-cy="1-column-annotation-card-standardized-variable-dropdown"] input')
+      .should('be.visible')
+      .and('have.value', 'some variable');
+    cy.get('[data-cy="1-column-annotation-card-data-type-categorical-button"]')
+      .should('be.visible')
+      .and('have.class', 'Mui-selected');
+    cy.get('[data-cy="2-column-annotation-card"]').should('be.visible');
+    cy.get('[data-cy="2-description"]')
+      .should('be.visible')
+      .and('contain', 'This is another column');
+    cy.get('[data-cy="2-column-annotation-card-standardized-variable-dropdown"] input')
+      .should('be.visible')
+      .and('have.value', '');
+    cy.get('[data-cy="2-column-annotation-card-data-type-continuous-button"]')
+      .should('be.visible')
+      .and('have.class', 'Mui-selected');
   });
-  it.only('toggles the data type between categorical and continuous', () => {
+  it('edits the description', () => {
+    cy.mount(<ColumnAnnotation />);
+    cy.get('[data-cy="1-edit-description-button"]').click();
+    cy.get('[data-cy="1-description-input"]').clear();
+    cy.get('[data-cy="1-description-input"]').type('new description');
+    cy.get('[data-cy="1-save-description-button"]').click();
+    cy.get('[data-cy="1-description"]').should('contain', 'new description');
+  });
+  it('toggles the data type between categorical and continuous', () => {
+    cy.mount(<ColumnAnnotation />);
+
     cy.get('[data-cy="1-column-annotation-card-data-type-categorical-button"]').should(
       'have.class',
       'Mui-selected'
@@ -14,7 +72,9 @@ describe('ColumnAnnotation', () => {
       'not.have.class',
       'Mui-selected'
     );
+
     cy.get('[data-cy="1-column-annotation-card-data-type-continuous-button"]').click();
+
     cy.get('[data-cy="1-column-annotation-card-data-type-continuous-button"]').should(
       'have.class',
       'Mui-selected'
