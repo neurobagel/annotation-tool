110d6ad35270730cad54e85f5112ec953a86b038
rmanaem <rmanaem@protonmail.ch>
2025-03-19 11:49:16 -0400

    Added a test case for the bug

--- cypress/component/ValueAnnotation.cy.tsx
+++ cypress/component/ValueAnnotation.cy.tsx
@@ -18,4 +18,30 @@ describe('ValueAnnotation', () => {
       .should('be.visible')
       .and('contain', 'Please select the appropriate data type for this column.');
   });
+  it('asserts that there is no shared state between EditDescription components in Continuous component', () => {
+    useDataStore.setState({ columns: mockColumnsWithDataType });
+    useDataStore.setState((state) => ({
+      columns: {
+        ...state.columns,
+        2: {
+          ...state.columns[2],
+          dataType: 'Continuous',
+        },
+      },
+    }));
+
+    cy.mount(<ValueAnnotation />);
+    cy.get('[data-cy="side-column-nav-bar-continuous-participant_id"]').click();
+    cy.get('[data-cy="1-continuous"]').should('be.visible');
+    cy.get('[data-cy="1-edit-description-button"]').click();
+    cy.get('[data-cy="1-description-input"]').clear();
+    cy.get('[data-cy="1-description-input"]').type('Years');
+    cy.get('[data-cy="1-save-description-button"]').click();
+    cy.get('[data-cy="side-column-nav-bar-continuous-participant_id"]').click();
+    cy.get('[data-cy="side-column-nav-bar-continuous-age"]').click();
+    cy.get('[data-cy="2-continuous"]').should('be.visible');
+    cy.get('[data-cy="2-description"]').should('contain', '');
+    cy.get('[data-cy="2-edit-description-button"]').click();
+    cy.get('[data-cy="2-description-input"]').should('not.contain', 'Years');
+  });
 });
