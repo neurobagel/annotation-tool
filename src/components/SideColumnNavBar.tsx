1d1c80070ca6c295a06573ab537c674a8fe2cd94
rmanaem <rmanaem@protonmail.ch>
2025-04-15 13:58:26 -0400

    Updated logic to display `Annotated` and `Unannotated` as top level elements

--- src/components/SideColumnNavBar.tsx
+++ src/components/SideColumnNavBar.tsx
@@ -1,18 +1,21 @@
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import { Typography, Paper, Collapse, Button, List, ListItemButton } from '@mui/material';
+import { Typography, Paper, Collapse, Button, List, ListItem, ListItemButton } from '@mui/material';
 import { useState } from 'react';
-import { Columns } from '../utils/types';
+import { Columns, StandardizedVariable } from '../utils/types';
+import { getMappedStandardizedVariables } from '../utils/util';
 
 interface ColumnTypeCollapseProps {
-  dataType: 'Categorical' | 'Continuous' | null;
+  dataType?: 'Categorical' | 'Continuous' | null;
+  standardizedVariable?: StandardizedVariable | null;
   columns: Columns;
   onSelectColumn: (columnId: string, columnDataType: 'Categorical' | 'Continuous' | null) => void;
   selectedColumnId: string | null;
 }
 
 function ColumnTypeCollapse({
-  dataType,
+  dataType = null,
+  standardizedVariable = null,
   columns,
   onSelectColumn,
   selectedColumnId,
@@ -22,17 +25,28 @@ function ColumnTypeCollapse({
   function toggleExpansion() {
     setExpanded((prev) => !prev);
   }
-  const dataTypeColumns = Object.entries(columns).filter(
-    dataType
-      ? ([_, column]) => column.dataType === dataType
-      : ([_, column]) => !column.dataType || column.dataType === null
-  );
-  const dataTypeLabel = dataType ? dataType.toLocaleLowerCase() : 'other';
+  let columnsToDisplay;
+  // TODO: find a better name for the below variable
+  let labelToDisplay;
+  if (standardizedVariable) {
+    columnsToDisplay = Object.entries(columns).filter(
+      ([_, column]) => column.standardizedVariable?.identifier === standardizedVariable.identifier
+    );
+    labelToDisplay = standardizedVariable.label;
+  } else {
+    columnsToDisplay = Object.entries(columns).filter(
+      dataType
+        ? ([_, column]) => column.standardizedVariable === null && column.dataType === dataType
+        : ([_, column]) =>
+            (column.standardizedVariable === null && !column.dataType) || column.dataType === null
+    );
+    labelToDisplay = dataType ? dataType.toLocaleLowerCase() : 'other';
+  }
 
   return (
-    <div data-cy={`side-column-nav-bar-${dataTypeLabel}`}>
+    <div data-cy={`side-column-nav-bar-${labelToDisplay}`}>
       <Button
-        data-cy={`side-column-nav-bar-${dataTypeLabel}-toggle-button`}
+        data-cy={`side-column-nav-bar-${labelToDisplay}-toggle-button`}
         className="justify-start"
         fullWidth
         onClick={() => toggleExpansion()}
@@ -41,19 +55,19 @@ function ColumnTypeCollapse({
         <Typography
           sx={{
             fontWeight:
-              selectedColumnId && dataTypeColumns.some(([id]) => id === selectedColumnId)
+              selectedColumnId && columnsToDisplay.some(([id]) => id === selectedColumnId)
                 ? 'bold'
                 : 'normal',
           }}
         >
-          {dataTypeLabel.charAt(0).toUpperCase() + dataTypeLabel.slice(1)}
+          {labelToDisplay.charAt(0).toUpperCase() + labelToDisplay.slice(1)}
         </Typography>
       </Button>
       <Collapse in={expanded}>
         <List>
-          {dataTypeColumns.map(([columnId, column]) => (
+          {columnsToDisplay.map(([columnId, column]) => (
             <ListItemButton
-              data-cy={`side-column-nav-bar-${dataTypeLabel}-${column.header}`}
+              data-cy={`side-column-nav-bar-${labelToDisplay}-${column.header}`}
               key={columnId}
               selected={selectedColumnId === columnId}
               onClick={() => onSelectColumn(columnId, dataType)}
@@ -79,30 +93,62 @@ interface SideColumnNavBarProps {
 }
 
 function SideColumnNavBar({ columns, onSelectColumn, selectedColumnId }: SideColumnNavBarProps) {
+  const mappedStandardizedVariables = getMappedStandardizedVariables(columns);
+  const [expanded, setExpanded] = useState<boolean>(true);
+  function toggleExpansion() {
+    setExpanded((prev) => !prev);
+  }
   /* 
   TODO: once configuration is in and we have a way to designate certain columns
   like participant_id and session_id as "doesn't need value annotation", we can filter them out here
   */
   return (
     <Paper className="w-full max-w-64 p-4" elevation={3} data-cy="side-column-nav-bar">
-      <ColumnTypeCollapse
-        dataType="Categorical"
-        columns={columns}
-        onSelectColumn={onSelectColumn}
-        selectedColumnId={selectedColumnId}
-      />
-      <ColumnTypeCollapse
-        dataType="Continuous"
-        columns={columns}
-        onSelectColumn={onSelectColumn}
-        selectedColumnId={selectedColumnId}
-      />
-      <ColumnTypeCollapse
-        dataType={null}
-        columns={columns}
-        onSelectColumn={onSelectColumn}
-        selectedColumnId={selectedColumnId}
-      />
+      <Button
+        className="justify-start"
+        fullWidth
+        onClick={() => toggleExpansion()}
+        endIcon={expanded ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+      >
+        <Typography>Annotated</Typography>
+      </Button>
+      <Collapse in={expanded}>
+        <List>
+          {mappedStandardizedVariables.map((standardizedVariable) => (
+            <ListItemButton>
+              <ColumnTypeCollapse
+                dataType={null}
+                standardizedVariable={standardizedVariable}
+                columns={columns}
+                onSelectColumn={onSelectColumn}
+                selectedColumnId={selectedColumnId}
+              />
+            </ListItemButton>
+          ))}
+        </List>
+      </Collapse>
+      <Button
+        className="justify-start"
+        fullWidth
+        onClick={() => toggleExpansion()}
+        endIcon={expanded ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+      >
+        <Typography>Unannotated</Typography>
+      </Button>
+      <Collapse in={expanded}>
+        <List>
+          {['Categorical', 'Continuous', null].map((dataType) => (
+            <ListItemButton>
+              <ColumnTypeCollapse
+                dataType={dataType as 'Categorical' | 'Continuous' | null}
+                columns={columns}
+                onSelectColumn={onSelectColumn}
+                selectedColumnId={selectedColumnId}
+              />
+            </ListItemButton>
+          ))}
+        </List>
+      </Collapse>
     </Paper>
   );
 }
