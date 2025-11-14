7fadc12c7299c6b94a0be085ef155a028964fb72
rmanaem <rmanaem@protonmail.ch>
2025-11-14 18:15:23 -0500

    Implemented `MultiColumnMeasureTabs` component

---
+++ src/components/MultiColumnMeasureTabs.tsx
@@ -0,0 +1,26 @@
+import { Tabs, Tab } from '@mui/material';
+import { SyntheticEvent } from 'react';
+import { StandardizedVariable } from '../../datamodel';
+
+interface MultiColumnMeasureTabsProps {
+  variables: StandardizedVariable[];
+  value: number;
+  onChange: (event: SyntheticEvent, newValue: number) => void;
+}
+
+function MultiColumnMeasureTabs({ variables, value, onChange }: MultiColumnMeasureTabsProps) {
+  return (
+    <Tabs value={value} onChange={onChange} aria-label="Multi-column measures tabs">
+      {variables.map((variable, index) => (
+        <Tab
+          data-cy={`multi-column-measures-tab-${variable.name}`}
+          key={variable.id}
+          label={variable.name}
+          id={`tab-${index}`}
+        />
+      ))}
+    </Tabs>
+  );
+}
+
+export default MultiColumnMeasureTabs;
