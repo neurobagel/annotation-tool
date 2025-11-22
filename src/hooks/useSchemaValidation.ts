707cf9a0db2751a681bb37db5b6760a6762c787f
rmanaem <rmanaem@protonmail.ch>
2025-11-22 15:08:38 -0500

    refactor: move `useSchemaValidation` hook to its own module

---
+++ src/hooks/useSchemaValidation.ts
@@ -0,0 +1,28 @@
+import Ajv from 'ajv';
+import { useMemo } from 'react';
+import { DataDictionary } from '../../datamodel';
+import schema from '../assets/neurobagel_data_dictionary.schema.json';
+
+export function useSchemaValidation(dataDictionary: DataDictionary) {
+  return useMemo(() => {
+    const ajv = new Ajv({ allErrors: true });
+    const validate = ajv.compile(schema);
+    const isValid = validate(dataDictionary);
+
+    if (!isValid) {
+      const errors =
+        validate.errors?.map((error) => {
+          const pathSegments = error.instancePath.slice(1).split('/');
+          return pathSegments[0];
+        }) || [];
+
+      const uniqueErrors = Array.from(new Set(errors));
+
+      return { schemaValid: false, schemaErrors: uniqueErrors };
+    }
+
+    return { schemaValid: true, schemaErrors: [] };
+  }, [dataDictionary]);
+}
+
+export default useSchemaValidation;
