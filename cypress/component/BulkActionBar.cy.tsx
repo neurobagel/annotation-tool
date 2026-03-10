7e952a5be52f287bbef260d7de9df7930c5512f4
rmanaem <rmanaem@protonmail.ch>
2026-03-10 12:26:23 -0400

    test: add test for unmapped selected state in BulkActionBar

--- cypress/component/BulkActionBar.cy.tsx
+++ cypress/component/BulkActionBar.cy.tsx
@@ -45,6 +45,24 @@ describe('BulkActionBar', () => {
     cy.get('[data-cy="bulk-unassign-mappings"]').should('not.be.disabled');
   });
 
+  it('should leave clear mappings disabled when columns are selected but none are mapped', () => {
+    cy.mount(
+      <BulkActionBar
+        selectedCount={3}
+        onClearSelection={props.onClearSelection}
+        onAssignDataType={props.onAssignDataType}
+        onClearMappings={props.onClearMappings}
+      />
+    );
+
+    cy.get('[data-cy="action-bar"]').should('be.visible').and('contain', '3 columns selected');
+    cy.get('[data-cy="clear-selection-button"]').should('not.be.disabled');
+    cy.get('[data-cy="bulk-assign-categorical"]').should('not.be.disabled');
+    cy.get('[data-cy="bulk-assign-continuous"]').should('not.be.disabled');
+    cy.get('[data-cy="bulk-assign-none"]').should('not.be.disabled');
+    cy.get('[data-cy="bulk-unassign-mappings"]').should('be.visible').and('be.disabled');
+  });
+
   it('should call onClearSelection with appropriate payload when clear button is clicked', () => {
     const onClearSelection = cy.stub().as('onClearSelection');
 
