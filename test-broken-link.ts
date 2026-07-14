67c0a6c507b61ae14c97384e23fe52533582bf60
rmanaem <rmanaem@protonmail.ch>
2026-07-14 02:13:17 -0400

    chore: remove placeholder test files and refine lychee ignore rules

--- test-broken-link.ts
+++
@@ -1,1 +0,0 @@
-const url = 'https://this-is-a-totally-fake-domain-that-does-not-exist.com/broken';
--- test-broken-link.tsx
+++
@@ -1,3 +0,0 @@
-export const BrokenLink = () => (
-  <a href="https://this-is-a-totally-fake-domain-that-does-not-exist.com/broken">Broken Link</a>
-);
